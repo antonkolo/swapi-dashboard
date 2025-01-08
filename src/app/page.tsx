1bde81fce33d2cbb90f7f4be11c74e5ca530390f
Anton Kolo <kvanton7@gmail.com>
2025-01-08 12:12:51 +0100

    Add simple fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+export default async function Home() {
+  const data = await fetch('https://swapi.py4e.com/api/people/');
+  const people = await data.json();
+  console.log(people);
+
   return (
     <div>
       <h1>The Force Directory</h1>
